c3f86dee3356e9ac83a07ddd81b3384b87100b48
pnyekrasov <pnekrasov1910@gmail.com>
2023-08-12 16:02:09 +0300

    HW 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+import { FeedbackWidget } from './FeedbackWidget/FeedbackWidget';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <FeedbackWidget />
     </div>
   );
 };
---
+++ src/components/FeedbackWidget/FeedbackWidget.jsx
@@ -0,0 +1,30 @@
+export const FeedbackWidget = () => {
+  return (
+    <section>
+      <h2>Please leave feedback</h2>
+      <ul>
+        <li>
+          <button>Good</button>
+        </li>
+        <li>
+          <button>Neutral</button>
+        </li>
+        <li>
+          <button>Bad</button>
+        </li>
+      </ul>
+      <h3>Statistics</h3>
+      <ul>
+        <li>
+          <p>Good</p>
+        </li>
+        <li>
+          <p>Neutral</p>
+        </li>
+        <li>
+          <p>Bad</p>
+        </li>
+      </ul>
+    </section>
+  );
+};
